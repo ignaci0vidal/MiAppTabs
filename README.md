041a12c42405b314f8f961b80283138b98bebf2a
ignacio vidal <ignaciovidal@gmail.com>
2026-04-30 14:22:59 -0300

    Crear proyecto Expo con TypeScript

--- README.md
+++
@@ -1,2 +0,0 @@
-# MiAppTabs
-TP - clase5 - Desarrollo de aplicaciones para dispositivos 1c2026 (DS)
---
+++ src/types/navigation.ts
@@ -0,0 +1,5 @@
+export type RootTabParamList = {
+  Inicio: undefined;
+  Buscar: undefined;
+  Perfil: undefined;
+};
